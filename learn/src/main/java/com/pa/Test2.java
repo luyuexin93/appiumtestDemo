e32fafbbe6809f4272067c270ccb1d4f5bd5e37d
Lyx <luyuexin93@outlook.com>
2020-03-20 11:59:40 +0800

    add appium-support dependency to start appium server

--- learn/src/main/java/com/pa/Test2.java
+++ learn/src/main/java/com/pa/Test2.java
@@ -1,12 +1,15 @@
 package com.pa;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
+import com.comcast.magicwand.spells.appium.dawg.utils.AppiumServerController;
+import com.github.genium_framework.appium.support.server.AppiumServer;
+import com.github.genium_framework.server.ServerArguments;
 import com.pa.controller.LoginController;
 import com.pa.util.ScreenShot;
 import io.appium.java_client.NetworkConnectionSetting;
@@ -38,6 +41,22 @@ public class Test2 {
 
 	  
 	  public static void main(String []args) throws Exception {
+		  	System.setProperty("nodepath", "D:\\Program Files\\nodejs\\node.exe");
+			System.setProperty("appiumjspath", "C:\\Users\\luyuexin\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js");
+			
+			File nodeFile = new File(System.getProperty("nodepath"));
+			File appiumjs = new File(System.getProperty("appiumjspath"));
+			ServerArguments serverArguments = new ServerArguments(); //初始化服务器参数类
+			AppiumServer aserver= new AppiumServer(nodeFile, appiumjs, serverArguments);
+			aserver.startServer(30000);
+			System.out.println(aserver.toString());
+			//appiumserver监听类
+			AppiumServerController act = new AppiumServerController();
+			System.out.println(act.checkServerState("127.0.0.1", 4723));
+//			aserver.stopServer();
+//			System.out.println(act.checkServerState("127.0.0.1", 4723));
+		  
+		  
 		  Test2.setAppium();
 		  LoginController.login(driver, "057105", "057105");
 //		  if(Element.isElementExist(LoginPage.login_Wait(driver))) {
