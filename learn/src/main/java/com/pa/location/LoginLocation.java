fd5738ffa782657bbcff0979a4f173e36c1fb1ae
luyuexin <luyuexin93@outlook.com>
2020-03-20 08:08:57 +0800

    add InitAppium father class

--- learn/src/main/java/com/pa/location/LoginLocation.java
+++ learn/src/main/java/com/pa/location/LoginLocation.java
@@ -1,10 +1,10 @@
 package com.pa.location;
 
-public class LoginLocation {
-	
-	public final static String ACCOUNTID_STRING = "account";
-	public final static String PASSWORDID_STRING = "password";
-	public final static String LOGINID_STRING = "login_click";
-	public final static String LOGIN_MESSAGE = "//android.widget.TextView[@resource-id='android:id/message']";
-	public final static String LOGIN_WAIT = "//android.widget.TextView[@resource-id='com.zjipst.pa:id/dialog_message'][contains(@text,'正在登录')]";
+public interface LoginLocation {
+
+	String ACCOUNTID_STRING = "account";
+	String PASSWORDID_STRING = "password";
+	String LOGINID_STRING = "login_click";
+	String LOGIN_MESSAGE = "//android.widget.TextView[@resource-id='android:id/message']";
+	String LOGIN_WAIT = "//android.widget.TextView[@resource-id='com.zjipst.pa:id/dialog_message'][contains(@text,'正在登录')]";
 }
--- learn/src/test/java/com/pa/test/InitAppium.java
+++ learn/src/test/java/com/pa/test/InitAppium.java
@@ -1,5 +1,116 @@
 package com.pa.test;
 
 public class InitAppium {
+	private static String automationName = "Appium"; //
+	private static String deviceName;
+	private static String platformName = "Android";
+	private static String platformVersion = "9";
+	private static String udid; // 设备唯一标识符
+	private static String appPath; // 应用apk路径
+	private static String appPackage; // 应用包名
+	private static String appActivity; // 启动的activity
+	private static String noReset; // 默认不重置应用 清空数据
+	private static String noSign;
+	private static String commandTimeout; // 命令超时时间
 
+	public InitAppium() {
+		this(new Builder());
+	}
+
+	// https://testerhome.com/topics/9904
+	public InitAppium(Builder builder) {
+		// TODO Auto-generated constructor stub
+		this.automationName = builder.automationName; //
+		this.deviceName = builder.deviceName;
+		this.platformName = builder.platformName;
+		this.platformVersion = builder.platformName;
+		this.udid = builder.udid; // 设备唯一标识符
+		this.appPath = builder.appPath; // 应用apk路径
+		this.appPackage = builder.appPackage; // 应用包名
+		this.appActivity = builder.appActivity; // 启动的activity
+		this.noReset = builder.noReset; // 默认不重置应用 清空数据
+		this.noSign = builder.noSign;
+		this.commandTimeout = builder.commandTimeout; // 命令超时时间
+	}
+
+	/**
+	 * @author lu 初始化默认参数内部类Builder
+	 */
+	public static class Builder {
+		private String automationName = "Appium"; //
+		private String deviceName = "S4";
+		private String platformName = "Android";
+		private String platformVersion = "9";
+		private String udid = "DWT7N19422002869"; // 设备唯一标识符
+		private String appPath; // 应用apk路径
+		private String appPackage = "com.zjipst.pa"; // 应用包名
+		private String appActivity = ".SplashActivity"; // 启动的activity
+		private String noReset = "True"; // 默认不重置应用 清空数据
+		private String noSign = "True";
+		private String commandTimeout = "30"; // 命令超时时间
+
+		public Builder() {
+		}
+
+		public Builder setAutomationName(String automationName) {
+			this.automationName = automationName;
+			return this;
+		}
+
+		public Builder setDeviceName(String deviceName) {
+			this.deviceName = deviceName;
+			return this;
+		}
+
+		public Builder setPlatformName(String platformName) {
+			this.platformName = platformName;
+			return this;
+		}
+
+		public Builder setPlatformVersion(String platformVersion) {
+			this.platformVersion = platformVersion;
+			return this;
+
+		}
+
+		public Builder setUdid(String udid) {
+			this.udid = udid;
+			return this;
+		}
+
+		public Builder setAppPathString(String appPathString) {
+			this.appPath = appPathString;
+			return null;
+		}
+
+		public Builder setAppPackage(String appPackage) {
+			this.appPackage = appPackage;
+			return this;
+		}
+
+		public Builder setAppActivity(String appActivity) {
+			this.appActivity = appActivity;
+			return this;
+		}
+
+		public Builder setNoReset(String noReset) {
+			this.noReset = noReset;
+			return this;
+		}
+
+		public Builder setNoSign(String noSign) {
+			this.noSign = noSign;
+			return this;
+		}
+
+		public Builder setCommandTimeout(String commandTimeout) {
+			this.commandTimeout = commandTimeout;
+			return this;
+		}
+
+		public InitAppium builder() {
+			return new InitAppium(this);
+		}
+
+	}
 }
