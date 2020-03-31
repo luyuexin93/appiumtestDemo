71084d229a650130aa405d6846e8e6c223caa22f
luyuexin <luyuexin93@outlook.com>
2020-03-31 21:33:01 +0800

    user基础rest api实现

---
+++ demoSpringboot/src/test/java/com/demo/springboot/DemoSpringbootApplicationTests.java
@@ -0,0 +1,36 @@
+package com.demo.springboot;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.demo.springboot.web.HelloController;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DemoSpringbootApplicationTests {
+
+	private MockMvc mvc;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect((ResultMatcher) content().string("Hello ,Spring Boot！"));
+	}
+
+}
---
+++ demoSpringboot/src/test/java/com/demo/springboot/web/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.demo.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloControllerTest {
+	@Test
+	public void testSayHello() {
+
+	}
+}
