f5cb32272a03578c075140bd722761f133ed3c53
luyuexin <luyuexin93@outlook.com>
2020-04-07 22:23:54 +0800

    TO-DO: fomat rest-response

---
+++ demoSpringboot/src/main/java/com/demo/springboot/common/RestResponse.java
@@ -0,0 +1,17 @@
+package com.demo.springboot.common;
+
+/** 
+* @author 作者luyuexin: 
+* @version 创建时间：2020年4月7日 下午10:11:24 
+* 类说明 
+*/
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(description = "返回结果")
+public class RestResponse {
+	private int code;
+	// TO-DO
+	// https://blog.csdn.net/weixin_34403693/article/details/91393634
+	// 格式化响应消息
+}
