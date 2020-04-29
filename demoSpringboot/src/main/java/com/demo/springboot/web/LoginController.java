0e871d5e2fff0a69bd013ad2b5945485760cbdde
luyuexin <luyuexin93@outlook.com>
2020-04-29 08:16:31 +0800

    feat: add encryptUtil

--- demoSpringboot/src/main/java/com/demo/springboot/web/LoginController.java
+++ demoSpringboot/src/main/java/com/demo/springboot/web/LoginController.java
@@ -2,10 +2,10 @@ package com.demo.springboot.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,7 +14,7 @@ import com.demo.springboot.entity.User;
 @RestController
 
 public class LoginController {
-	@GetMapping(value = {"/"})
+	@GetMapping(value = { "/" })
 	public ModelAndView index(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
 
@@ -22,24 +22,26 @@ public class LoginController {
 		if (ObjectUtils.isEmpty(user)) {
 			mv.setViewName("redirect:/login");
 		} else {
-			mv.setViewName("page/index");
-			mv.addObject(user);
+			if (StringUtils.isNotBlank(user.getName())) {
+				mv.setViewName("page/index");
+				mv.addObject(user);
+			} else {
+				mv.setViewName("redirect:/login");
+			}
 		}
 
 		return mv;
 	}
 
-	
-	
 	@PostMapping("/login")
-	public ModelAndView login(User user,HttpServletRequest request) {
+	public ModelAndView login(User user, HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
 		mv.addObject(user);
 		mv.setViewName("redirect:/");
 		request.getSession().setAttribute("user", user);
 		return mv;
 	}
-	
+
 	@GetMapping("/login")
 	public ModelAndView login() {
 		return new ModelAndView("page/login");
